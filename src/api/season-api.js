806d9d25a640d1dc00835d3eee7eb3981d16daef
chrisberry4545 <chrisberry4545@googlemail.com>
2018-11-23 12:21:38 +0000

    added some error handling

--- src/api/season-api.js
+++ src/api/season-api.js
@@ -7,13 +7,21 @@ const {
 
 module.exports = (router = new Router()) => {
   router.get('/', async (req, res) => {
-    const results = await fetchAllSeasonData();
-    return res.json(results);
+    try {
+      const results = await fetchAllSeasonData();
+      return res.json(results);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
   });
   router.get('/:seasonName', async (req, res) => {
     const { seasonName } = req.params;
-    const result = await fetchSeasonDataBySeasonName(seasonName);
-    return res.json(result);
+    try {
+      const result = await fetchSeasonDataBySeasonName(seasonName);
+      return res.json(result);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
   });
   return router;
 };
