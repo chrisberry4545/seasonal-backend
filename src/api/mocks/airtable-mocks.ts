7134f0be370a150d803326444078799b8633ec94
chrisberry4545 <chrisberry4545@googlemail.com>
2019-03-14 18:02:12 +0000

    added some basic unit tests for the apis

---
+++ src/api/mocks/airtable-mocks.ts
@@ -0,0 +1,78 @@
+import {
+  Airtable,
+  AirtableBaseRecord,
+  AirtableInstance,
+  AirtableSelectQuery
+} from '../../interfaces';
+import { AIRTABLE_TABLES } from '../../const';
+
+import mockFoodTableData from './data/food-table.data.json';
+import mockRecipeTableData from './data/recipe-table.data.json';
+import mockSeasonData from './data/season-table.data.json';
+
+const convertToRecord = <T extends AirtableBaseRecord>(item: T) => {
+  return {
+    id: item.id,
+    get: jest.fn().mockImplementation((propertyName: keyof T) => {
+      return item[propertyName];
+    })
+  }
+};
+
+const initAirtableObject = <T extends AirtableBaseRecord>(
+  airtableData: T[]
+) => {
+  return {
+    select: jest.fn().mockImplementation((
+      select: AirtableSelectQuery<AirtableBaseRecord>
+    ) => {
+      const isSelectEquals = /(.*)\s=\s(.*)/;
+      const selectEqualsResult =
+        isSelectEquals.exec(select.filterByFormula || '');
+      const dataToUse = selectEqualsResult && selectEqualsResult.length > 2
+        ? airtableData.filter((data) => {
+          const matchingField = (data as any)[selectEqualsResult[1]];
+          return matchingField === undefined ||
+            matchingField.toString() === selectEqualsResult[2]
+        })
+        : airtableData;
+      const selectReturnValue = {
+        eachPage:
+          jest.fn().mockImplementation((perPageCallback, onCompleteCallback) => {
+            const convertedData = dataToUse.map(convertToRecord);
+            perPageCallback(convertedData, () => {});
+            onCompleteCallback();
+          })
+      };
+      return selectReturnValue;
+    })
+  };
+};
+
+const getDataForTableName = (tableName: string) => {
+  switch(tableName) {
+    case AIRTABLE_TABLES.FOOD:
+      return mockFoodTableData;
+    case AIRTABLE_TABLES.RECIPES:
+      return mockRecipeTableData;
+  }
+  return mockSeasonData;
+};
+
+export const initMockAirtableBaseResult = () => (
+  tableName: string
+): AirtableInstance => (
+  initAirtableObject<AirtableBaseRecord>(getDataForTableName(tableName))
+);
+
+const mockAirtable: Airtable = {
+  base: jest.fn().mockReturnValue(initMockAirtableBaseResult())
+}
+
+export const airtableMockSetup = () => {
+  jest.mock('airtable', () => {
+    return function() {
+      return mockAirtable;
+    }
+  });
+};
---
+++ src/api/season-api.spec.ts
@@ -0,0 +1,79 @@
+import {
+  airtableMockSetup
+} from './mocks';
+airtableMockSetup();
+
+import {
+  app,
+} from '../app';
+
+import supertest from 'supertest';
+
+const seasonUrl = process.env.SEASON_DATA_ENDPOINT;
+
+describe('Get all seasons', () => {
+  const makeAllSeasonRequest = () => {
+    return supertest(app).get(`/${seasonUrl}`);
+  };
+
+  test('Returns a status of 200', async () => {
+    const result = await makeAllSeasonRequest();
+    expect(result.status).toBe(200);
+  });
+
+  test('Returns a full list of season data', async () => {
+    const result = await makeAllSeasonRequest();
+    expect(result.body).toMatchSnapshot();
+  });
+
+  test('Returns the name of the seasons', async () => {
+    const result = await makeAllSeasonRequest();
+    expect(result.body[0].name).toBe('January');
+    expect(result.body[1].name).toBe('February');
+  });
+});
+
+describe('Get single season', () => {
+  const makeSingleSeasonRequest = (id: string = '0') => {
+    return supertest(app).get(`/${seasonUrl}/${id}`);
+  };
+
+  const makeSeasonRequestWithFoodAndRecipes = () => {
+    return makeSingleSeasonRequest('1');
+  };
+
+  test('Returns a status of 200', async () => {
+    const result = await makeSingleSeasonRequest();
+    expect(result.status).toBe(200);
+  });
+
+  test('Retrieves a single season', async() => {
+    const result = await makeSingleSeasonRequest();
+    expect(result.body).toMatchSnapshot();
+  });
+
+  test('Retrieves a single season with food and recipe data', async() => {
+    const result = await makeSeasonRequestWithFoodAndRecipes();
+    expect(result.body).toMatchSnapshot();
+  });
+
+  test('Does not populate a seasons recipes if they do not exist', async () => {
+    const result = await makeSingleSeasonRequest();
+    expect(result.body.recipes).toBeUndefined();
+  });
+
+  test('Populates a seasons recipes if they exist', async () => {
+    const result = await makeSeasonRequestWithFoodAndRecipes();
+    expect(result.body.recipes.length > 0).toBe(true);
+  });
+
+  test('Does not populate a seasons food if they do not exist', async () => {
+    const result = await makeSingleSeasonRequest();
+    expect(result.body.food).toBeUndefined();
+  });
+
+  test('Populates a seasons food if they exist', async () => {
+    const result = await makeSeasonRequestWithFoodAndRecipes();
+    expect(result.body.food.length > 0).toBe(true);
+  });
+});
