ce919861226ae451eed0ac2db74f9d11b7d02db0
chrisberry4545 <chrisberry4545@googlemail.com>
2019-12-22 16:20:31 +0000

    added food name mappings to get seasons with food query

--- src/sql/get-seasons-with-food.sql
+++ src/sql/get-seasons-with-food.sql
@@ -1,3 +1,19 @@
+WITH
+	food_name_mappings AS (
+		SELECT
+      country_to_food_name_map.food_id,
+      country_to_food_name_map.name
+		FROM country_to_food_name_map
+		WHERE country_to_food_name_map.country_id = ANY(
+      SELECT
+        regions.country_id
+      FROM
+        regions
+      WHERE
+        regions.code = $1
+		)
+	)
+
 SELECT
   id,
   season_index,
@@ -12,10 +28,22 @@ SELECT
         SELECT json_agg(
           json_build_object(
             'id', food.id,
-            'name' , food.name,
+            'name' , COALESCE(
+              (
+                SELECT food_name_mappings.name FROM food_name_mappings
+                WHERE food_name_mappings.food_id = food.id
+              ),
+              food.name
+            ),
             'imageUrlSmall', food.image_url_small
           )
-          ORDER BY food.name
+          ORDER BY COALESCE(
+            (
+              SELECT food_name_mappings.name FROM food_name_mappings
+              WHERE food_name_mappings.food_id = food.id
+            ),
+            food.name
+          )
         ) AS food
         FROM food
         LEFT JOIN region_to_season_food_map
