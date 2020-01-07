81e5eb1db6050dbce944a05a9550aed439371f55
chrisberry4545 <chrisberry4545@googlemail.com>
2020-01-07 14:49:16 +0000

    tests working locally

--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,14 @@
 #! /bin/bash
 
-#set +e
+docker-compose down
+echo "Starting tests..."
+
+docker-compose run --rm app-test-ci
+status=$?
 
 docker-compose down
-#docker-compose up -d
-echo "Starting tests..."
-sleep 10
 
-#docker-compose run app-test-ci npm run test
-run_result=$(docker-compose run --rm app-test-ci)
-
-echo "Run result:"
-echo $run_result
-
-#docker-compose down
-
-if [ "$run_result" = "0" ]; then
+if [ "$status" = "0" ]; then
   echo "Tests passed"
   exit 0;
 fi
